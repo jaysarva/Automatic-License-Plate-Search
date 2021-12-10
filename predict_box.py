21d1109d73cc83f27cb65ce34b3862e6d1326241
pbooncho <55765957+pbooncho@users.noreply.github.com>
2021-12-10 17:09:15 -0500

    Update predict_box.py

--- predict_box.py
+++ predict_box.py
@@ -3,10 +3,11 @@ import numpy as np
 from skimage.io import imread
 import matplotlib.pyplot as plt
 import os
+import shutil
 from PIL import Image
 import cv2
 
-path, dirs, files = next(os.walk("test_data/preprocessed_data/resized_images"))
+path, dirs, files = next(os.walk("test_data/preprocessed_data"))
 n = len(files)
 my_model = load_model("my_model")
 
@@ -53,7 +54,7 @@ def calculate_rough_accuracy():
     #print(rows)
     total = 0
     for i in range(n):
-        image_path = "test_data/preprocessed_data/resized_images/Cars" + str(i) + ".png"
+        image_path = "test_data/preprocessed_data/Cars" + str(i) + ".png"
         [image_path, startY, startX, endY, endX] = predict_image(image_path,my_model)
         row = rows[i]
         
@@ -66,7 +67,15 @@ def calculate_rough_accuracy():
         image = imread(image_path)
         cv2.rectangle(image, (startX, startY), (endX, endY),(255, 255, 0), 1)
         im = Image.fromarray(image)
-        im.save("test_licenses/license"+str(i)+".png")
+
+        licenses_path = "test_licenses"
+        isExist = os.path.exists(licenses_path)
+        if not isExist:
+            os.makedirs(licenses_path)
+        else:
+            shutil.rmtree(licenses_path)
+            os.makedirs(licenses_path)
+            im.save("test_licenses/license"+str(i)+".png")
     
     print("Accuracy = " + str(total / n))
                  
