11d7c1cda9d3e547dfafd297065d87d4b562675a
pbooncho <55765957+pbooncho@users.noreply.github.com>
2021-12-12 21:02:48 -0500

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -77,7 +77,7 @@ def predict(images):
 def convert_result(num):
     real = ['1', '2', '3', '4', '5', '6', '7', '8', '9', "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W" \
         "X", "Y", "Z"]
-    return real[num]
+    return real[int(num)]
 
 #train()
 
